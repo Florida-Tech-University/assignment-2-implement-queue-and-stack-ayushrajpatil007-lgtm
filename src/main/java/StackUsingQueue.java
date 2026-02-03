a6d9b49e4c8917325267c29a9490bae1167e383b
ayushrajpatil007-lgtm <ayushrajpatil007@gmail.com>
2026-02-04 00:21:10 +0530

    Update StackUsingQueue.java

--- src/main/java/StackUsingQueue.java
+++ src/main/java/StackUsingQueue.java
@@ -1,38 +1,66 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class StackUsingQueue implements StackADT {
 
-    private Queue<Integer> q = new LinkedList<>();
+    private ArrayQueue q1;
+    private ArrayQueue q2;
+    private int capacity;
+    private int size;
 
-    @Override
-    public void push(int x) {
-        q.add(x);
-        int size = q.size();
-        for (int i = 1; i < size; i++) {
-            q.add(q.remove());
-        }
-    }
-
-    @Override
-    public int pop() {
-        if (q.isEmpty()) {
-            return -1; // REQUIRED
-        }
-        return q.remove();
-    }
-
-    @Override
-    public int peek() {
-        if (q.isEmpty()) {
-            return -1; // REQUIRED
-        }
-        return q.peek();
+    public StackUsingQueue(int capacity) {
+        this.capacity = capacity;
+        q1 = new ArrayQueue(capacity);
+        q2 = new ArrayQueue(capacity);
+        size = 0;
     }
 
     @Override
     public boolean isEmpty() {
-        return q.isEmpty();
+        return size == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    @Override
+    public void push(int x) {
+        if (isFull()) {
+            return; // NO exception
+        }
+
+        q2.enqueue(x);
+
+        while (!q1.isEmpty()) {
+            q2.enqueue(q1.dequeue());
+        }
+
+        ArrayQueue temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        size++;
+    }
+
+    @Override
+    public int pop() {
+        if (isEmpty()) {
+            return -1;
+        }
+        size--;
+        return q1.dequeue();
+    }
+
+    @Override
+    public int peek() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return q1.peek();
+    }
+
+    @Override
+    public int size() {
+        return size;
     }
 }
 
