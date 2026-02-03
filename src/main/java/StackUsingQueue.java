70de1d5f0e8076469b3c3fc951f1091a5a5e7497
ayushrajpatil007-lgtm <ayushrajpatil007@gmail.com>
2026-02-04 00:00:53 +0530

    Update StackUsingQueue.java

--- src/main/java/StackUsingQueue.java
+++ src/main/java/StackUsingQueue.java
@@ -1,65 +1,28 @@
-public class ArrayQueue implements QueueADT {
+import java.util.LinkedList;
+import java.util.Queue;
 
-    private int[] arr;
-    private int front, rear, size, capacity;
+public class StackUsingQueue implements StackADT {
 
-    public ArrayQueue(int capacity) {
+    private Queue<Integer> q;
+    private int capacity;
+
+    public StackUsingQueue(int capacity) {
         this.capacity = capacity;
-        arr = new int[capacity];
-        front = 0;
-        rear = -1;
-        size = 0;
+        q = new LinkedList<>();
     }
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return q.isEmpty();
     }
 
     @Override
     public boolean isFull() {
-        return size == capacity;
+        return q.size() == capacity;
     }
 
-    @Override
-    public void enqueue(int x) {
-        if (isFull()) {
-            throw new IllegalStateException("Queue is full");
-        }
-        rear = (rear + 1) % capacity;
-        arr[rear] = x;
-        size++;
-    }
+    @Overrid
 
-    @Override
-    public int dequeue() {
-        if (isEmpty()) {
-            throw new IllegalStateException("Queue is empty");
-        }
-        int val = arr[front];
-        front = (front + 1) % capacity;
-        size--;
-        return val;
-    }
-
-    @Override
-    public int peek() {
-        if (isEmpty()) {
-            throw new IllegalStateException("Queue is empty");
-        }
-        return arr[front];
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public int capacity() {
-        return capacity;
-    }
-}
 
 
 
